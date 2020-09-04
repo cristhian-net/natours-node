103ec49ad1d49d83c1f0437b994eb0d0a6a6e445
Cristhian Valencia <valenciacristhian47@gmail.com>
2020-09-04 01:34:44 -0300

    get tours

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from express', app: 'Natours' });
 });
 
+const tours = require('./dev-data/data/tours.json')
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
 app.post('/', (req, res) => {
   res.send('You can send requests to this endpoint');
 });
